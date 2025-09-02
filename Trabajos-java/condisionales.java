b05e7d2ac442a5d82e98f68047234562e9979f36
Juan Esteban Cardona <juan.cardonacal@campusucc.edu.co>
2025-09-02 03:30:39 +0000

    cambio

--- Trabajos-java/condisionales.java
+++ Trabajos-java/condisionales.java
@@ -1,5 +1,20 @@
 import java.util.scanner;
 
 public class condisionales {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+        System.out.println("ingrese otro numero: ");
+        int numero_2 = scanner.nextInt();
+
+        if (numero > numero_2) {
+            System.out.println("El primer numero es el mayor");
+        } else if (numero < numero_2) {
+            System.out.println("El primer numero es el mas pequeño");
+        } else {
+            System.out.println("Los numeros son iguales");
+        }
+
 
 }
