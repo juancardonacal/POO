d4cd00430eb4b65973b65133456b31f4c295f4e8
Juan Esteban Cardona <juan.cardonacal@campusucc.edu.co>
2025-09-02 15:06:08 +0000

    inicio de ejercicio 5.1

--- Trabajos-java/trabajos_bucle/cinco_uno.java
+++ Trabajos-java/trabajos_bucle/cinco_uno.java
@@ -1,1 +1,15 @@
+import java.util.Scanner
 
+public class cinco_uno {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int numero = 0, contador = 0;
+
+        do {
+            
+            
+        }
+    }
+}
