6f70a161e3043577c385ea53b315c20ccaeb79c2
Juan Esteban Cardona <juan.cardonacal@campusucc.edu.co>
2025-09-23 01:01:21 +0000

    guardar

--- Practica de POO/Ejecutar_persona.java
+++ Practica de POO/Ejecutar_persona.java
@@ -20,11 +20,17 @@ public class Ejecutar_persona {
 
         // Instanciar el objeto (new) --> Instanciar es igual a crear (Darle vida al objeto)
 
-        objPersona1 = new Persona("maicol", "hijodejack", 40, 1123445460, 65.5);
+        objPersona1 = new Persona("maicol", "hijodejack", 40, 1123445460, 65.5, 1.80);
 
         //Forma#2
-        Persona objPersona2 = new Persona("Byron", "Cano", 35, 1001980, 60.0);
+        Persona objPersona2 = new Persona("Byron", "Cano", 35, 1001980, 60.0, 1.75);
 
+        // personas creadas para la tarea.
+        Persona objPersona3 = new Persona("Carles", "Sans", 28, 987654321, 75.0, 1.80);
+
+        Persona objPersona4 = new Persona("Anacleta", "Spuns", 22, 123456789, 55.0, 1.65);
+
+        Persona objPersona5 = new Persona("Fernando", "Fernandez", 30, 456789123, 80.0, 1.75);
 
         // Mostrar los objetos creados
         System.out.println(objPersona1.toString());
@@ -33,15 +39,49 @@ public class Ejecutar_persona {
 
         // Mostrar el nombre de la persona 1
         System.out.println(objPersona1.getNombre());
+        // Moatrar el apellido de la persona 1
+        System.out.println(objPersona1.getApellido());
+        // Mostrar la edad de la persona 1
+        System.out.println(objPersona1.getEdad());
+        // Mostrar la cedula de la persona 1
+        System.out.println(objPersona1.getCedula());
+        // Mostrar el peso de la persona 1
+        System.out.println(objPersona1.getPeso());
+        // Mostrar la altura de la persona 1
+        System.out.println(objPersona1.getAltura());
 
         // Modificar el nombre de la persona 2
         objPersona2.setNombre("Harvin");
+        // Modificar el apellido de la persona 2
+        objPersona2.setApellido("Noches");
+        // Modificar la edad de la persona 2
+        objPersona2.setEdad(30);
+        // Modificar la cedula de la persona 2
+        objPersona2.setCedula(1234567890);
+        // Modificar el peso de la persona 2
+        objPersona2.setPeso(70.0);
+        // Modificar la altura de la persona 2
+        objPersona2.setAltura(1.85);
 
-        // Mostrar el objPersona2 con la modificacion del nombre
+        // Mostrar el objPersona2 con las modificaciones
         System.out.println(objPersona2.toString());
 
+        // Mostrar las personas creadas para la tarea
+        System.out.println(objPersona3.toString()); 
+        System.out.println(objPersona4.toString());
+        System.out.println(objPersona5.toString());
 
-        
+        // Este es el metodo saludar.
+        System.out.println(objPersona1.saludar());
+
+        // Este es el metodo para saber si la persona es mayor de edad.
+        System.out.println(objPersona1.getNombre() + " es mayor de edad: " + objPersona1.esMayorDeEdad());
+
+        // Este es el metodo para calcular el IMC.
+        System.out.println("El IMC de " + objPersona1.getNombre() + " es: " + objPersona1.calcularIMC());
+
+
+
 
     }
     
--- Practica de POO/Persona.java
+++ Practica de POO/Persona.java
@@ -6,33 +6,93 @@ public class Persona {
     private int edad;
     private int cedula;
     private double peso;
+    private double altura;
 
     // Metodo contructor de la clase
     // El constructor de la clase permite inicalizar la clase
     // El constructor de una clase se reconoce porque tiene el mismo nombre de la
     // clase
     // El operador this es una autorefencia al atributo de la clase
-    public Persona(String nombre, String apellido, int edad, int cedula, double peso) {
+    public Persona(String nombre, String apellido, int edad, int cedula, double peso, double altura) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
         this.cedula = cedula;
         this.peso = peso;
+        this.altura = altura;
     }
     // El metodo toString sirve para visualizar los objetos.
     public String toString() {
         return "persona [Nombre: " + nombre + " Apellido: " + apellido + " Edad: " + edad + " Cedula: " + cedula
-                + " Peso: " + peso + "]";
+                + " Peso: " + peso + " Altura: " + altura + "]";
     }
 
     // Metodos Getters y Setters
     public String getNombre() { // Obtener el nombre
         return nombre;
-     }
+    }
 
+    public String getApellido() { // Obtener el apellido
+        return apellido;
+    }
+
+    public int getEdad() { // Obtener la edad
+        return edad;
+    }
+
+    public int getCedula() { // Obtener la cedula
+        return cedula;
+    }
+
+    public double getPeso() { // Obtener el peso
+        return peso;
+    }
+
+    public double getAltura() { // Obtener la altura
+        return altura;
+    }
+
+    // Metodos Setters
     public void setNombre(String nombre) { // modificar el nombre
         this.nombre = nombre;
      }
-    
 
+    public void setApellido(String apellido) { // modificar el apellido
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) { // modificar la edad
+        this.edad = edad;
+    }
+
+    public void setCedula(int cedula) { // modificar la cedula
+        this.cedula = cedula;
+    }
+
+    public void setPeso(double peso) { // modificar el peso
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) { // modificar la altura
+        this.altura = altura;
+     }
+
+    // Este metodo es para saludar.
+    public String saludar() {
+        return "Hola, " + nombre;
+    }
+
+    // Este metodo es para saber si es mayor de edad.
+    public boolean esMayorDeEdad() {
+        if (edad >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // este metodo es para calcular el IMC (Indice de Masa Corporal) de la persona.
+    public double calcularIMC() {
+        return peso / (altura * altura);
+    }
 }
