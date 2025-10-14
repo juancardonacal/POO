66c38c42570a9d1903a6012a80821940d8cbc973
Juan Esteban Cardona <juan.cardonacal@campusucc.edu.co>
2025-10-14 05:11:05 +0000

    organizando los trabajos

--- Trabajos-java/trabajos_bucle/cinco_uno.java
+++ Trabajos-java/trabajos_bucle/cinco_uno.java
@@ -1,4 +1,4 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class cinco_uno {
     public static void main(String[] args) {
@@ -8,8 +8,12 @@ public class cinco_uno {
         int numero = 0, contador = 0;
 
         do {
-            
-            
-        }
+            System.out.print("Introduce un número: ");
+            numero = input.nextInt();
+            System.out.println("Has introducido: " + numero);
+            contador++;
+        } while (contador < 5); // Example condition, replace as needed
+
+        input.close();
     }
 }
